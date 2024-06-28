4169aa57d6780ebb53a2d11044c30f67fb836a31
Jørgen Bøe Svendsen <jorgenbs@gmail.com>
2024-06-28 11:59:06 +0200

    fix: påfyll sometimes contains non-quiz articles, use filter

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,13 +3,15 @@
 ARCHIVE_FILE="./archive.json"
 FEEDSIZE=1
 
-# {"_id":"","content_alias":"pafyll","feature":"top-table-list","from":0,"size":1}
-QUERY="%7B%22_id%22%3A%22%22%2C%22content_alias%22%3A%22pafyll%22%2C%22feature%22%3A%22top-table-list%22%2C%22from%22%3A0%2C%22size%22%3A1%7D"
+# {"_id":"","content_alias":"pafyll","feature":"top-table-list","from":0,"size":4}
+QUERY="%7B%22_id%22%3A%22%22%2C%22content_alias%22%3A%22pafyll%22%2C%22feature%22%3A%22top-table-list%22%2C%22from%22%3A0%2C%22size%22%3A4%7D"
 # {content_elements{_id,content_restrictions{content_code},display_date,embed_html,headlines{basic,original,print,web},subtype,type,websites{morgenbladet{website_section{_id,name},website_url}}}}
 FILTER="%7Bcontent_elements%7B_id%2Ccontent_restrictions%7Bcontent_code%7D%2Cdisplay_date%2Cembed_html%2Cheadlines%7Bbasic%2Coriginal%2Cprint%2Cweb%7D%2Csubtype%2Ctype%2Cwebsites%7Bmorgenbladet%7Bwebsite_section%7B_id%2Cname%7D%2Cwebsite_url%7D%7D%7D%7D"
 API_CALL="https://www.morgenbladet.no/pf/api/v3/content/fetch/mentor-api-collections?query=${QUERY}&filter=${FILTER}&_website=morgenbladet"
 
-DATA=$(curl $API_CALL | jq ".content_elements[0]")
+# filter by subtype quiz and take first element
+DATA=$(curl $API_CALL | jq '[.content_elements[] | select(.subtype=="quiz")][0]')
+
 
 ID=$(echo $DATA | jq -r "._id")
 NAME=$(echo $DATA | jq -r ".headlines.basic")
@@ -20,6 +22,12 @@ if ! jq --arg id "$ID" -e '.[] | select(._id == $id)' $ARCHIVE_FILE > /dev/null;
     ARTICLE_URL=https://www.morgenbladet.no$(echo $DATA | jq -r ".websites.morgenbladet.website_url")
     echo $ARTICLE_URL
     SLUG=$(curl $ARTICLE_URL | sed -n 's/.*kviss\.morgenbladet\.no\/\([^"]*\)".*/\1/p' | sed 's/\\//g')
+
+    # no slug means something went wrong
+    if [ -z "$SLUG" ]; then
+        echo "No slug found"
+        exit 1
+    fi
 
     # # Update archive
     TEMP_FILE=$(mktemp)
